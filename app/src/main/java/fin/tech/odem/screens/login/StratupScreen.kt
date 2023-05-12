a7f8b28e4121c4f7a4fb1d2a3d7b5235b6136682
GhoulamMB <mohdz0683@gmail.com>
2023-05-12 13:12:48 +0200

    Updated Auto login

--- app/src/main/java/fin/tech/odem/screens/login/StratupScreen.kt
+++ app/src/main/java/fin/tech/odem/screens/login/StratupScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,7 +20,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
@@ -29,7 +29,6 @@ import fin.tech.odem.screens.destinations.HomeViewDestination
 import fin.tech.odem.screens.destinations.LoginViewDestination
 import fin.tech.odem.screens.destinations.RegisterViewDestination
 import fin.tech.odem.viewModels.LoginViewModel
-import kotlinx.coroutines.launch
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @RootNavGraph(start = true)
@@ -38,34 +37,36 @@ import kotlinx.coroutines.launch
 fun Startup (navigator:DestinationsNavigator){
     val loginViewModel = viewModel<LoginViewModel>()
     val token = loginViewModel.getToken()
-    if (token != null) {
-        loginViewModel.viewModelScope.launch {
+    LaunchedEffect(token) {
+        if (token != null) {
             loginViewModel.loginWithToken(token)
             navigator.navigate(HomeViewDestination)
         }
     }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Welcome to Odem \uD83D\uDC4B", color = Color.White)
-        Spacer(modifier = Modifier.padding(vertical = 24.dp))
-        Image(painter = painterResource(id = R.drawable.onlinepayment), contentDescription = "Payments logo",modifier = Modifier.size(256.dp))
-        Spacer(modifier = Modifier.padding(vertical = 24.dp))
-        Button(onClick = { navigator.navigate(direction = LoginViewDestination) },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF536DFE))) {
+    if(token == null){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Welcome to Odem \uD83D\uDC4B", color = Color.White)
+            Spacer(modifier = Modifier.padding(vertical = 24.dp))
+            Image(painter = painterResource(id = R.drawable.onlinepayment), contentDescription = "Payments logo",modifier = Modifier.size(256.dp))
+            Spacer(modifier = Modifier.padding(vertical = 24.dp))
+            Button(onClick = { navigator.navigate(direction = LoginViewDestination) },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF536DFE))) {
 
-            Text(text = "Login", fontSize = 24.sp,
-                modifier = Modifier.size(width = 128.dp, height = 36.dp),
-                textAlign = TextAlign.Center)
-        }
-        Spacer(modifier = Modifier.padding(vertical = 24.dp))
-        Button(onClick = { navigator.navigate(direction = RegisterViewDestination) },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF536DFE))) {
-            Text(text = "Register", fontSize = 24.sp,
-                modifier = Modifier.size(width = 128.dp, height = 36.dp),
-                textAlign = TextAlign.Center)
+                Text(text = "Login", fontSize = 24.sp,
+                    modifier = Modifier.size(width = 128.dp, height = 36.dp),
+                    textAlign = TextAlign.Center)
+            }
+            Spacer(modifier = Modifier.padding(vertical = 24.dp))
+            Button(onClick = { navigator.navigate(direction = RegisterViewDestination) },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF536DFE))) {
+                Text(text = "Register", fontSize = 24.sp,
+                    modifier = Modifier.size(width = 128.dp, height = 36.dp),
+                    textAlign = TextAlign.Center)
+            }
         }
     }
 }
